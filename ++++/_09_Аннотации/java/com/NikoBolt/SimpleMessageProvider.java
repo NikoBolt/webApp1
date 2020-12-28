90ffc838ff4068f4a9a419280101f491993068fc
NikoBolt <docnikbolt@gmail.com>
2020-12-28 20:29:26 +0300

    Аннотация в JavaConfig сама ищет классы с @Component и создает бины

---
+++ ++++/_09_Аннотации/java/com/NikoBolt/SimpleMessageProvider.java
@@ -0,0 +1,13 @@
+package com.NikoBolt;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleMessageProvider implements MessageProvider {
+
+    @Override
+    public String getMessage() {
+        return "Hello world";
+    }
+
+}
